6a64923a54c8fcea248b313dc81f2aded04579a5
Moustapha DIOP <moustephdiop@gmail.com>
2023-11-11 20:43:12 +0100

    (add) documentation to function

--- descent/functions.py
+++ descent/functions.py
@@ -1,12 +1,24 @@
 import numpy as np
 import math
+from typing import Tuple
 
+def create_system(dim: int, cond: int = 10, seed: int = 100) -> Tuple[np.array, np.array]:
+    """
+    Creates a system of linear equations Ax = b with the given dimensions and condition number.
 
-def create_system(dim, cond: int = 10, seed: int = 100):
+    Args:
+        dim (int): The dimension of the system.
+        cond (int, optional): The condition number of the system. Defaults to 10.
+        seed (int, optional): The seed for the random number generator. Defaults to 100.
+
+    Returns:
+        Tuple[np.array, np.array]: The matrix A and the vector b of the system.
+    """
+
     rng = np.random.default_rng(seed)
     cond_sqrt = math.sqrt(cond)
     
-    A = 0.1 * rng.uniform(-cond_sqrt, cond_sqrt, size=(dim,dim))
+    A = 0.1 * rng.uniform(-cond_sqrt, cond_sqrt, size=(dim, dim))
     A = np.triu(A)
     
     # replace the diagonal of A with random positive values between 1 and sqrt(cond)
@@ -19,4 +31,4 @@ def create_system(dim, cond: int = 10, seed: int = 100):
     
     b = 1. * rng.integers(-10, 10, size=(dim))
     
-    return A,b
+    return A, b
