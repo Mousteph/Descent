3a3a347d2656941306b1925b8d31c61d42eb49a4
Moustapha DIOP <moustephdiop@gmail.com>
2023-11-12 19:26:47 +0100

    (add) gradient descent fletcher reeves

--- descent/gradient/gd_fletcher_reeves.py
+++ descent/gradient/gd_fletcher_reeves.py
@@ -1,34 +1,67 @@
 from .gd_optimal_step import GradientDescentOptimalStep
 import numpy as np
+from .helpers import gradient
+from typing import Callable
 
 
 class GradientDescentFletcherReeves(GradientDescentOptimalStep):
-    def gradient_compose_dk_FR(self, f, xk, x0, dk):
-        grad = self.gradient(f, xk)
+    def gradient_compose_dk_FR(self, f: Callable[[np.array], float], xk: np.array,
+                               x0: np.array, dk: np.array) -> np.array:
+        """
+        Compute the gradient composition for Fletcher-Reeves method.
+
+        Args:
+            f (Callable[[np.array], float]): The function to optimize.
+            xk (np.array): The current point in the optimization process.
+            x0 (np.array): The initial point in the optimization process.
+            dk (np.array): The current direction in the optimization process.
+
+        Returns:
+            np.array: The new direction for the next step in the optimization process.
+        """
+
+        grad = gradient(f, xk)
     
-        return -grad + (np.linalg.norm(grad) ** 2 / np.linalg.norm(self.gradient(f, x0))**2) * dk
+        return -grad + (np.linalg.norm(grad) ** 2 / np.linalg.norm(gradient(f, x0))**2) * dk
     
-    def __call__(self, f, pk, eps=1E-6, max_iter=10000, detect_div=10e5, error=False):
-        dk = -self.gradient(f, pk)
-        pk1 = pk + self.backtrack(pk, f, dk) * dk
-        l = [pk, pk1]
+    def __call__(self, f: Callable[[np.array], float], x0: np.array, eps: float = 1E-6,
+                 max_iter: int = 10000, detect_div: float = 10e5):
+        """
+        Perform the optimization process using the Fletcher-Reeves method.
+
+        Args:
+            f (Callable[[np.array], float]): The function to optimize.
+            x0 (np.array): The initial point in the optimization process.
+            eps (float, optional): The precision of the solution. Defaults to 1E-6.
+            max_iter (int, optional): The maximum number of iterations. Defaults to 10000.
+            detect_div (float, optional): The divergence detection threshold. Defaults to 10e5.
+
+        Returns:
+            np.array: The points visited during the optimization process.
+        """
+
+        dk = -gradient(f, x0)
+        mu = self.backtrack(x0, f, dk)
+        pk1, pk = x0, x0 + mu * dk
+        
+        points = [pk1, pk]
         i = 0
         
         norm = np.linalg.norm(pk1 - pk, 2)
         
-        while i < max_iter and (norm >= eps and norm <= detect_div):
-            dk = self.gradient_compose_dk_FR(f, pk1, pk, dk)
-            pk = pk1
-            pk1 = pk + self.backtrack(pk, f, dk) * dk
+        while i < max_iter and (eps <= norm <= detect_div):
+            dk = self.gradient_compose_dk_FR(f, pk, pk1, dk)
+            mu = self.backtrack(pk, f, dk)
+            pk1, pk = pk, pk + mu * dk
             
             norm = np.linalg.norm(pk1 - pk, 2)
-            l.append(pk)
+            points.append(pk)
             i += 1
             
-        l = np.array(l)
+        points = np.array(points)
         
         self._check_max_iter(i, max_iter)
         self._check_norm(norm, detect_div)
-        self._set_report(f(l[-1]), i)
+        self._set_report(f(points[-1]), i)
         
-        return l
+        return points
